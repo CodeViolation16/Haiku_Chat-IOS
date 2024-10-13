978467aa329bf77cc4e09ee42dba5d9892044b5e
khanh <ngiakhanh18@gmail.com>
2024-10-13 18:45:18 -0500

    Initial Commit

---
+++ Haiku_Chat/Haiku_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haiku_ChatApp.swift
+//  Haiku_Chat
+//
+//  Created by Khanh Nguyen on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Haiku_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
